92917d49352fb24a3db8f7d113d22396c301db4e
Gover-dev <tvoj.smertt@mail.ru>
2024-05-20 08:17:08 +0500

    sing in

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>login</title>
+</head>
+<body>
+    <form class="wrapper" action="login.php" method="post">
+        <?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+        <div class="center logo_from">
+            <img src="assets/img/logo.png">
+
+            <div class="w-100"></div>
+        </div>
+        <div class="w-100"></div>
+        <div class="mb-3 ">
+            <input type="text" class="form-control" name="login" placeholder="Логин">
+        </div>
+
+        <div class="w-100"></div>
+        <div class="mb-3 ">
+            <input type="password" class="form-control" name="password" placeholder="Пароль">
+        </div>
+
+        <div class="w-100"></div>
+        <div class="mb-3 center">
+            <button type="submit" class="btn col-md-4">Войти</button>
+        </div>
+    </form>
+    
+</body>
+</html>
